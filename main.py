0a1b56315ac5d1c94dd55d1829fa37b5806e3a76
NathMiller <nathanmiller5304@gmail.com>
2024-04-11 12:58:40 +0100

    variables established

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 
+document = "the quick brown brown brown brown brown fox"
+seek = input("Please enter the word you want replaced: ")
+replace1 = input("Please enter the first replacement string: ")
+replace2 = input("Please enter the second replacement string: ")
 
