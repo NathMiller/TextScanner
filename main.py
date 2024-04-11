f2233332cb0aaad4abb264f880d7ddc493b0f785
NathMiller <nathanmiller5304@gmail.com>
2024-04-11 14:09:44 +0100

    Created a loop that finds the keyword and inserts its place into an array along with which word it should be replaced with.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-
-document = "the quick brown brown brown brown brown fox"
+document = "the quick brown brown brown brown brown brown brown brown fox"
 seek = input("Please enter the word you want replaced: ")
 replace1 = input("Please enter the first replacement string: ")
 replace2 = input("Please enter the second replacement string: ")
+
+seekQuantity = document.count(seek)
+instances = []
+
+for i in range(seekQuantity):
+    if i % 2 == 0:
+        instances.insert(i, [i, replace1])
+
+    else:
+        instances.insert(i, [i, replace2])
+
+print(instances)
 
