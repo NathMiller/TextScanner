0f365ef4507cf2d55f9294d3f59dc77832ccd4dc
NathMiller <nathanmiller5304@gmail.com>
2024-04-11 14:28:01 +0100

    Replaces each required word one a time, alternating between the two selected replacements. Array goes unused in this process, however can be used in the future to keep track of document changes.

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-document = "the quick brown brown brown brown brown brown brown brown fox"
+document = "the quick brown brown brown fox"
 seek = input("Please enter the word you want replaced: ")
 replace1 = input("Please enter the first replacement string: ")
 replace2 = input("Please enter the second replacement string: ")
 
 seekQuantity = document.count(seek)
+updatedDoc = document
 instances = []
 
 for i in range(seekQuantity):
     if i % 2 == 0:
         instances.insert(i, [i, replace1])
-
+        updatedDoc = updatedDoc.replace(seek, replace1, 1)
     else:
         instances.insert(i, [i, replace2])
+        updatedDoc = updatedDoc.replace(seek, replace2, 1)
 
 print(instances)
-
+print(updatedDoc)
